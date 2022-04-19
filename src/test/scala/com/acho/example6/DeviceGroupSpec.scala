65a34a6f10459bce62db7e7f29b67edb77c49255
acho01 <achosharashenidze@gmail.com>
2022-04-19 14:31:22 +0400

    add ignore message tests

--- src/test/scala/com/acho/example6/DeviceGroupSpec.scala
+++ src/test/scala/com/acho/example6/DeviceGroupSpec.scala
@@ -5,6 +5,8 @@ import com.acho.example6.Device.{ReadTemperature, RecordTemperature, RespondTemp
 import com.acho.example6.DeviceManager.{DeviceRegistered, RequestTrackDevice}
 import org.scalatest.wordspec.AnyWordSpecLike
 
+import scala.concurrent.duration.DurationInt
+
 class DeviceGroupSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 
   "successfully register new device" in {
@@ -29,4 +31,14 @@ class DeviceGroupSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
     deviceActor2 ! RecordTemperature(2, 20.5, recorderProbe.ref)
     recorderProbe.expectMessage(TemperatureRecorded(2))
   }
+
+  "ignore wrong groupId requests" in {
+    val commandProbe = createTestProbe[DeviceRegistered]()
+    val deviceGroup = spawn(DeviceGroup("group-1"))
+
+    deviceGroup ! RequestTrackDevice("group-2", "device-1", commandProbe.ref)
+
+    commandProbe.expectNoMessage(1000.milli)
+  }
+
 }
