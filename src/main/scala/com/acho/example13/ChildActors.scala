7b991df70e06f3915c715d88746f7151d4a1a6d2
acho01 <achosharashenidze@gmail.com>
2022-05-18 18:28:25 +0400

    add actor watch example

--- src/main/scala/com/acho/example13/ChildActors.scala
+++ src/main/scala/com/acho/example13/ChildActors.scala
@@ -1,9 +1,9 @@
 package com.acho.example13
 
 import akka.NotUsed
-import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Terminated}
 import akka.actor.typed.scaladsl.Behaviors
-import com.acho.example13.ChildActors.Parent.{CreateChild, StopChild, TellChild}
+import com.acho.example13.ChildActors.Parent.{CreateChild, StopChild, TellChild, WatchChild}
 
 object ChildActors {
 
@@ -17,7 +17,11 @@ object ChildActors {
 
     case class StopChild() extends Command
 
-    def apply(): Behavior[Command] = Behaviors.receive { (context, message) =>
+    case class WatchChild() extends Command
+
+    def apply(): Behavior[Command] = idle()
+
+    def idle(): Behavior[Command] = Behaviors.receive { (context, message) =>
       message match {
         case CreateChild(name) =>
           context.log.info(s"Creating child $name")
@@ -27,7 +31,7 @@ object ChildActors {
     }
 
     def active(childRef: ActorRef[String]): Behavior[Command] =
-      Behaviors.receive { (context, message) =>
+      Behaviors.receive[Command] { (context, message) =>
         message match {
           case TellChild(msg) =>
             context.log.info(s"Telling child ${childRef.path.name} $msg")
@@ -36,12 +40,23 @@ object ChildActors {
           case StopChild() =>
             context.log.info(s"Stopping child ${childRef.path}")
             context.stop(childRef)
-            apply()
+            idle()
+          case WatchChild() => {
+            context.watch(childRef)
+            context.log.info(s"[Parent] watching child ${childRef.path.name}")
+            Behaviors.same
+          }
           case _ =>
             context.log.info("Unknown Message")
             Behaviors.same
         }
       }
+        .receiveSignal {
+          case (context, Terminated(stoppedChild)) => {
+            context.log.info(s"[Parent] child ${stoppedChild.path.name} was killed by ...")
+            idle()
+          }
+        }
   }
 
   object Child {
@@ -55,6 +70,7 @@ object ChildActors {
     val parent = context.spawn(Parent(), "parent")
     parent ! CreateChild("child-1")
     parent ! TellChild("Hi Child")
+    parent ! WatchChild()
     parent ! StopChild()
     parent ! CreateChild("child-2")
     parent ! TellChild("Ola Amigo")
